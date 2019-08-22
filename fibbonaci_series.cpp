565bfa17d3db3ea2ef7e6718fc8b8b50cab92f1b
suman saurabh <catchsaurabh0209@gmail.com>
2019-08-22 15:13:01 +0530

    searching commit

--- fibbonaci_series.cpp
+++ fibbonaci_series.cpp
@@ -1,4 +1,30 @@
-//program to perform the gcd to two no.
+//program to print the fibbonacci series.
 #include<stdio.h>
 #include<conio.h>
+int fibbo(int n);
+int main()
+{
+	int n,i;
+	printf("enter the range");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("%d",fibbo(i));
+	}
+}
+int fibbo(int n)
+{
+	if (n==0)
+	{
+		return 0;
+    }
+    else if (n==1)
+    {
+    	return 1;
+	}
+    else
+    {
+    	return (fibbo(n-1)+fibbo(n-2));
+	}
+}
 
---
+++ gcd.cpp
@@ -0,0 +1,9 @@
+//program to perform gcd of twwo no.
+#include<stdio.h>
+int main()
+{
+	int n1 , n2;
+	printf("Enter the no.");
+	scanf("%d%d",&n1,&n2);
+	hcf(n1,n2);
+}
