90056122d250b4b4f69825c139b1f10ebd7a728d
RManish76 <44274324+RManish76@users.noreply.github.com>
2019-09-05 20:18:28 +0530

    Add files via upload

---
+++ stacks.cpp
@@ -0,0 +1,23 @@
+//STACKS
+#include<stdio.h>
+int main()
+{
+	int ar[10],a,i=0, ch;
+	printf("Enter your choice\n 1 - PUSH\n 2 - POP\n 3 - DISPLAY\n 4 - EXIT");
+	scanf("%d",&ch);
+	switch(ch):
+		case(1): printf("Enter the no. to be pushed");
+				 push();
+				 break;
+		case(2): pop();
+				 break;
+		case(3): display();
+				 break;
+		case(4): exit();
+			     break;
+			     
+}
+int push(int a)
+{
+	
+}
